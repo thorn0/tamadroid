c152292b3e7b67dda46eaf217be498977d3133c6
mirus <motriz@i.ua>
2013-10-26 14:31:43 +0300

    total progress bar draft

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,5 +6,15 @@ angular.module("tamadroidApp").controller("MainCtrl", function($scope, $interval
 		$scope.rotate += 1;
 	}, 500, 10);
 
-    $scope.name = "Tamadroid";
+    $scope.name = "YourName";
+
+    $scope.install = function() {
+        //test action
+        $scope.name = "Qwerty";
+    }
+
+    $scope.var = {
+        current: 90,
+        total: 100
+    };
 });
