672659143910730d9de4d1d881bb5ca2c3a9b093
Oleksandr Pidlisnyi <linmas.org@gmail.com>
2013-10-27 11:06:35 +0200

    Android versions added

--- app/scripts/services/firmwaredb.js
+++ app/scripts/services/firmwaredb.js
@@ -25,5 +25,39 @@ angular.module("tamadroidApp").factory("firmwareDb", function() {
 		name: "Android 2.3 Gingerbread"
 	}, {
 		name: "Android 2.3.1 Gingerbread"
-	}, ];
+	}, {
+		name: "Android 2.3.2 Gingerbread"
+	}, {
+		name: "Android 2.3.3 Gingerbread"
+	}, {
+		name: "Android 2.3.4 Gingerbread"
+	}, {
+		name: "Android 2.3.5 Gingerbread"
+	}, {
+		name: "Android 2.3.6 Gingerbread"
+	}, {
+		name: "Android 3.0 Honeycomb"
+	}, {
+		name: "Android 3.1 Honeycomb"
+	}, {
+		name: "Android 3.2 Honeycomb"
+	}, {
+		name: "Android 4.0 Ice Cream Sandwich"
+	}, {
+		name: "Android 4.0.1 Ice Cream Sandwich"
+	}, {
+		name: "Android 4.0.2 Ice Cream Sandwich"
+	}, {
+		name: "Android 4.0.3 Ice Cream Sandwich"
+	}, {
+		name: "Android 4.0.4 Ice Cream Sandwich"
+	}, {
+		name: "Android 4.1 Jelly Bean"
+	}, {
+		name: "Android 4.2 Jelly Bean"
+	}, {
+		name: "Android 4.3 Jelly Bean"
+	}, {
+		name: "Android 4.4 KitKat"
+	}];
 });
